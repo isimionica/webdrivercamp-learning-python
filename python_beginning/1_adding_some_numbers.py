0fede743e6de530e004c67c52d4e32e1020a8a9b
Carolina <iuliaiurescu87@gmail.com>
2023-10-17 16:26:58 -0400

    Second print commit

---
+++ python_beginning/1_adding_some_numbers.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+number = 100
+print ('Learning Python is fun\"\' -{} %'.format(number))
+print ('Learning Python is fun\"\' - {number} %')
+print ('Learning Python is fun\"\' -', number, '%')
