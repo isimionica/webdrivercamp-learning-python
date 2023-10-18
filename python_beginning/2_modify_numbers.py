5c7cc16f878c3bd7ce66be56e1f0ead1e8f3bdcf
Carolina <iuliaiurescu87@gmail.com>
2023-10-18 17:47:48 -0400

    My third commit

---
+++ python_beginning/2_modify_numbers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+a = 3.141592653589793238
+print('Learning Python is fun\"\' - %.2f %%' % a)
+print('Learning Python is fun\"\' -{:.2f} %'.format(a))
+print(f'Learning Python is fun\"\' -{a:.2f} % ')
+
+
+
+
+
+
+
