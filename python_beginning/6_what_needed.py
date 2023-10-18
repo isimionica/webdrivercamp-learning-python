5d9185a1734b6b088c91d528a67f63ee494933cf
Carolina <iuliaiurescu87@gmail.com>
2023-10-18 18:31:57 -0400

    My seventh commit

---
+++ python_beginning/6_what_needed.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+var = 'There should be one-- and preferably only one --obvious way to do it. lthough that way may not be obvious at first unless you\'re Dutch.'
+var = 'preferably only one way unless'
+print(var)
