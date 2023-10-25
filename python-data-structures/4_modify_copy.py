7e7b649cdb0ee365fbfb7fd5e81a0e5959c2e350
Carolina <iuliaiurescu87@gmail.com>
2023-10-24 20:12:24 -0400

    Python modify and copy

---
+++ python-data-structures/4_modify_copy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+list_ = [5, 4, 3, 2, 1]
+index = 1
+element_to_replace = 5
+list_origin = list_.copy()
+if 0<=1<=len(list_):
+        list_[1] = 5
+        print(f"Copy: {list_}")
+print(f"Original: {list_origin}", )
+
